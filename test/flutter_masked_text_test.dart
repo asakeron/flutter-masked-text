372681dc06d7d91d95fc44c22d5d96fca429bf0a
André Silva <andre.silva@iea.sp.gov.local>
2022-07-07 16:26:25 -0300

    rewrite implementation to be null safe

--- test/flutter_masked_text_test.dart
+++ test/flutter_masked_text_test.dart
@@ -5,7 +5,7 @@ void main() {
   group('masked text', () {
     test('12345678901 with mask 000.000.000-00 results 123.456.789-01', () {
       var cpfController =
-          new MaskedTextController(text: '12345678901', mask: '000.000.000-00');
+          MaskedTextController(text: '12345678901', mask: '000.000.000-00');
 
       expect(cpfController.text, '123.456.789-01');
     });
@@ -14,7 +14,7 @@ void main() {
         '12345678901 with mask 000.000.000-00 and changed results 123.456.789.01',
         () {
       var cpfController =
-          new MaskedTextController(text: '12345678901', mask: '000.000.000-00');
+          MaskedTextController(text: '12345678901', mask: '000.000.000-00');
 
       expect(cpfController.text, '123.456.789-01');
 
@@ -24,26 +24,26 @@ void main() {
     });
 
     test('abc123 with mask AAA results abc', () {
-      var controller = new MaskedTextController(text: 'abc123', mask: 'AAA');
+      var controller = MaskedTextController(text: 'abc123', mask: 'AAA');
 
       expect(controller.text, 'abc');
     });
 
     test('update text to 123456 and mask 000-000 results on 123-456', () {
-      var controller = new MaskedTextController(text: '', mask: '000-000');
+      var controller = MaskedTextController(text: '', mask: '000-000');
       controller.updateText('123456');
 
       expect(controller.text, '123-456');
     });
 
     test('* must accept all characters', () {
-      var controller = new MaskedTextController(text: 'a0&#', mask: '****');
+      var controller = MaskedTextController(text: 'a0&#', mask: '****');
 
       expect(controller.text, 'a0&#');
     });
 
     test('@ must accept only letters and numbers', () {
-      var controller = new MaskedTextController(text: 'a0&#', mask: '@@@');
+      var controller = MaskedTextController(text: 'a0&#', mask: '@@@');
 
       expect(controller.text, 'a0');
     });
@@ -52,7 +52,7 @@ void main() {
       var translator = MaskedTextController.getDefaultTranslator();
       translator.remove('*');
 
-      var controller = new MaskedTextController(
+      var controller = MaskedTextController(
           mask: '0000 **** **** 0000', translator: translator);
       controller.updateText('12345678');
 
@@ -63,7 +63,7 @@ void main() {
       var translator = MaskedTextController.getDefaultTranslator();
       translator.remove('*');
 
-      var controller = new MaskedTextController(
+      var controller = MaskedTextController(
           mask: '0000 **** **** 0000', translator: translator);
       controller.updateText('12345678');
 
@@ -73,14 +73,14 @@ void main() {
 
   group('money mask', () {
     test('0.01 results 0,01', () {
-      var controller = new MoneyMaskedTextController();
+      var controller = MoneyMaskedTextController();
       controller.updateValue(0.01);
 
       expect(controller.text, '0,01');
     });
 
     test('1234.56 results 1.234,56', () {
-      var controller = new MoneyMaskedTextController();
+      var controller = MoneyMaskedTextController();
 
       controller.updateValue(1234.56);
 
@@ -88,21 +88,21 @@ void main() {
     });
 
     test('123123.0 results 123.123,00', () {
-      var controller = new MoneyMaskedTextController();
+      var controller = MoneyMaskedTextController();
       controller.updateValue(123123.0);
 
       expect(controller.text, '123.123,00');
     });
 
     test('1231231.0 results 1.231.231,00', () {
-      var controller = new MoneyMaskedTextController();
+      var controller = MoneyMaskedTextController();
       controller.updateValue(1231231.0);
 
       expect(controller.text, '1.231.231,00');
     });
 
     test('custom decimal and thousando separator results in 1,234.00', () {
-      var controller = new MoneyMaskedTextController(
+      var controller = MoneyMaskedTextController(
           decimalSeparator: '.', thousandSeparator: ',');
       controller.updateValue(1234.0);
 
@@ -110,7 +110,7 @@ void main() {
     });
 
     test('number value for 0,10 must be 0.1', () {
-      var controller = new MoneyMaskedTextController(
+      var controller = MoneyMaskedTextController(
           decimalSeparator: '.', thousandSeparator: ',');
       controller.updateValue(0.1);
 
@@ -118,16 +118,16 @@ void main() {
     });
 
     test('rightSymbol " US\$" and value 99.99 must resut in 99,99 US\$', () {
-      var controller = new MoneyMaskedTextController(rightSymbol: ' US\$');
+      var controller = MoneyMaskedTextController(rightSymbol: ' US\$');
       controller.updateValue(99.99);
 
       expect(controller.text, '99,99 US\$');
     });
 
     test('rightSymbol with number must raises an error.', () {
-      Function executor = () {
-        new MoneyMaskedTextController(rightSymbol: ' U4');
-      };
+      void executor() {
+        MoneyMaskedTextController(rightSymbol: ' U4');
+      }
 
       expect(executor, throwsArgumentError);
     });
@@ -135,21 +135,21 @@ void main() {
     test(
         'rightSymbol " US\$" with 12345678901234 must results in 123.456.789.012,34 US\$',
         () {
-      var controller = new MoneyMaskedTextController(rightSymbol: ' US\$');
+      var controller = MoneyMaskedTextController(rightSymbol: ' US\$');
       controller.updateValue(123456789012.34);
 
       expect(controller.text, '123.456.789.012,34 US\$');
     });
 
     test('leftSymbol "R\$ " and value 123.45 results in "R\$ 123,45"', () {
-      var controller = new MoneyMaskedTextController(leftSymbol: 'R\$ ');
+      var controller = MoneyMaskedTextController(leftSymbol: 'R\$ ');
       controller.updateValue(123.45);
 
       expect(controller.text, 'R\$ 123,45');
     });
 
     test('precision 3 and value 123.45 results in "123,450"', () {
-      var controller = new MoneyMaskedTextController(precision: 3);
+      var controller = MoneyMaskedTextController(precision: 3);
       controller.updateValue(123.45);
 
       expect(controller.text, '123,450');
